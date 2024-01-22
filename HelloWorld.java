e7c692020f311ebfa758d38cb562e74f317f0c39
yingkongxiong <133197363+yingkongxiong@users.noreply.github.com>
2024-01-22 14:40:27 -0500

    Revert "Updated HelloWorld"
    This reverts commit 7f6183e0605a2f1789cbd2805b6c66e9960bf71c.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld{
 
    public static void main(String[] args){
       System.out.println("Hello world!");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop");
       
    }
    
